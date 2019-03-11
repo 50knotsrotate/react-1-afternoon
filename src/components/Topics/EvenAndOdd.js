9c3df6991d9d2791dbebf233ed543acb87ba7549
= <=>
2019-03-10 23:00:00 -0700

    Added full functionality to Evens and Odds component

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
-class EvenAndOdd extends Component { 
+
+class EvenAndOdd extends Component {
+
+    state = {
+        array: '',
+        evensArray: undefined,
+        oddsArray: undefined
+    }
+
+    updateArray = (event) => {
+        var newArray = event.target.value.split(',').map(number => parseInt(number))
+
+        this.setState({
+            array: newArray
+        })
+    }
+
+    submitArray = () => { 
+        var oddsArray = [...this.state.array].filter(number => number % 2 == 1);
+        var evensArray = [...this.state.array].filter(number => number % 2 == 0)
+        
+        this.setState({
+            oddsArray,
+            evensArray,
+            array: ''
+        })
+        document.getElementsByClassName('inputLine')[0].value = ''
+    }
+
+
     render() { 
         return (
-            <h1>Even and Odd</h1>
+          <div className="puzzleBox evenAndOddPB">
+            <h4> Evens and Odds </h4>
+                <input className="inputLine" onChange={this.updateArray}></input>
+                <button className="confirmationButton" onClick={this.submitArray}> Split </button>
+                <span className="resultsBox"> Evens: {JSON.stringify(this.state.evensArray)} </span>
+                <span className="resultsBox"> Odds: {JSON.stringify(this.state.oddsArray)}  </span>
+          </div>
+             
         )
     }
 }
