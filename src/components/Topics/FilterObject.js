3f6c19549809f060fb5bad36e4f95455838f3571
= <=>
2019-03-10 23:51:17 -0700

    added functionality to FilterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react';
 
 
-class FilterObject extends Component { 
+class FilterObject extends Component {
+
+    state = {
+        keyWord: '',
+        originalObject:
+            [
+                {
+                    name: 'Declan',
+                    breed: 'Irish Wolfhound',
+                    is_good_boy: true,
+                    is_mamas_boy: true
+                },
+                {
+                    name: 'Musgy',
+                    likes_batteries: true,
+                    is_bork: true
+                }
+          
+            ],
+        updatedObject: undefined
+    }
+    updateKeyword = (e) => { 
+        this.setState({
+            keyWord: e.target.value
+        })
+
+    }
+    
+    getObjects = () => { 
+        let filteredObjects = [...this.state.originalObject]
+        filteredObjects =  filteredObjects.filter(dog => dog[this.state.keyWord]) 
+console.log(filteredObjects)
+        this.setState({
+            updatedObject: filteredObjects
+        })
+    }
+
     render() { 
         return (
-            <h1>Filter Object</h1>
+            <div className='puzzleBox filterObjectPB'>
+                <span className='puzzleText'> {JSON.stringify(this.state.originalObject)}</span>
+                <input className='inputLine' onChange={this.updateKeyword}></input>
+                <button className='confirmationButton' onClick={this.getObjects}>I'll filter your object ;)</button>
+                <span className='resultsBox filterObjectRB'>{JSON.stringify(this.state.updatedObject)}</span>
+            
+            </div>
+            
         )
     }
 }
